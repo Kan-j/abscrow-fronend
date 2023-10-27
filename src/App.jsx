5974137b09f9a95704692b59d8f33906e5ad6046
Kan-j <kwasiaddonyarkojunior@gmail.com>
2023-10-27 10:58:53 +0000

    Changes made

--- src/App.jsx
+++ src/App.jsx
@@ -17,27 +17,11 @@ import {
 
 function App() {
 
-<<<<<<< HEAD
   return (
     <>
       <LandingPage/>
     </>
   )
-=======
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="login" element={<Login />} />
-        <Route exact path="signup" element={<SignUp />} />
-        <Route exact path="adminpanel" element={<AdminPanel />} />
-        <Route exact path="orders" element={<AdminOrders />} />
-        <Route exact path="transaction-progress" element={<TransactionProgress />} />
-      </>
-    )
-  );
-  return <RouterProvider router={router} />;
->>>>>>> b2f6344452ea806fdb5f7c3136b0c7449d43e97c
 }
 
 export default App
